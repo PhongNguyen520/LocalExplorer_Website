b7dc090c1cc296b7a31f168b0abe7c54cf4adf56
PhongNguyen <PhongNTSE173175@fpt.edu.vn>
2025-07-06 10:05:30 +0700

    Fix Get Location

--- src/api/business/LocationAPI.jsx
+++ src/api/business/LocationAPI.jsx
@@ -1,3 +1,3 @@
-export const getProvinces = () => fetch('https://provinces.open-api.vn/api/p/').then(res => res.json());
-export const getDistricts = (provinceCode) => fetch(`https://provinces.open-api.vn/api/p/${provinceCode}?depth=2`).then(res => res.json());
-export const getWards = (districtCode) => fetch(`https://provinces.open-api.vn/api/d/${districtCode}?depth=2`).then(res => res.json()); 
+export const getProvinces = () => fetch('http://provinces.open-api.vn/api/p/').then(res => res.json());
+export const getDistricts = (provinceCode) => fetch(`http://provinces.open-api.vn/api/p/${provinceCode}?depth=2`).then(res => res.json());
+export const getWards = (districtCode) => fetch(`http://provinces.open-api.vn/api/d/${districtCode}?depth=2`).then(res => res.json()); 
